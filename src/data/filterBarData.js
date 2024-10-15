bb6295bd337176acf9f85af65f333c862990b431
Nguyen Anh Thang Dev <mnguyenanhthang2002@gmail.com>
2024-10-15 22:13:36 +0700

    first commit

---
+++ src/data/filterBarData.js
@@ -0,0 +1,63 @@
+export const sortMenu = [
+    {
+        id: 1,
+        title: "Latest",
+    },
+    {
+        id: 2,
+        title: "Featured",
+    },
+    {
+        id: 3,
+        title: "Top Rated",
+    },
+    {
+        id: 4,
+        title: "Price(Lowest First)",
+    },
+    {
+        id: 5,
+        title: "Price(Highest First)"
+    },
+];
+
+export const brandsMenu = [
+    {
+        id: 1,
+        label: "Lacoste",
+        checked: false,
+    },
+    {
+        id: 2,
+        label: "Dior",
+        checked: false,
+    },
+    {
+        id: 3,
+        label: "Adias",
+        checked: false,
+    },
+];
+
+export const categoryMenu = [
+    {
+        id: 1,
+        label: "Shirt",
+        checked: false,
+    },
+    {
+        id: 2,
+        label: "T-shirt",
+        checked: false,
+    },
+    {
+        id: 3,
+        label: "Jacket",
+        checked: false,
+    },
+    {
+        id: 4,
+        label: "Shorts",
+        checked: false,
+    },
+];
